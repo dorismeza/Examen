8d0cf7ec2829548c034b09a61bf8a04807028660
admin <admin@DORISMEZA>
2020-06-09 22:59:56 -0600

    espacio

--- Examen/registro.vb
+++ Examen/registro.vb
@@ -2,7 +2,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         Try
-            If txtusuario.Text = "Administrador " And txtcontraseña.Text = "SinHacer2020" Then
+            If txtusuario.Text = "Administrador" And txtcontraseña.Text = "SinHacer2020" Then
                 Form2.Show()
                 Me.Close()
             Else
